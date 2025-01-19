932ccd70d7cee32f3fbbbd6ec06fa77cc94d2322
shane <shane.mander31@gmail.com>
2025-01-18 21:20:56 -0800

    socketio

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 CORS(app)
+socket = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def hello_world():
@@ -12,3 +13,12 @@ def hello_world():
 @app.post("/esp32")
 def esp32():
   return "Received esp32 memory data"
+
+@socket.on('get_data')
+def handle_message(data = "testing socket"):
+    print(f"Received message: {data}")
+    emit('response', data)
+
+
+if __name__ == '__main__':
+    socket.run(app, debug=False)
